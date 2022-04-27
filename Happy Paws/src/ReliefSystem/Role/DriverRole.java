6cb10646087e463fad375dd130483f9fcf3e76b2
jsher <jsher@10.110.74.194>
2022-04-26 20:43:04 -0400

    DriverRole

---
+++ Happy Paws/src/ReliefSystem/Role/DriverRole.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ReliefSystem.Role;
+
+/**
+ *
+ * @author jsher
+ */
+public class DriverRole {
+    
+}
