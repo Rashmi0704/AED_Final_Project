57d691f84568fa15f8043396ec1f97c56d4f1a7d
shereenjohn <john.sh@northeastern.edu>
2022-04-27 10:25:24 -0400

    PetVolunteer

---
+++ HappyPaws_Final 7/src/HappyPawSystem/Role/PetVolunteerRole.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HappyPawSystem.Role;
+
+import javax.swing.JPanel;
+import HappyPawSystem.Ecosystem;
+import HappyPawSystem.UserAccount.UserAccount;
+import UserInterface.HospiitalAdmin.HospitalAdminWorkArea;
+import UserInterface.PetVolunteer.PetVolunteer;
+
+/**
+ *
+ * @author jsher
+ */
+public class PetVolunteerRole extends Role{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Ecosystem system) {
+          return new PetVolunteer(userProcessContainer, account, system);
+    }
+}
+
