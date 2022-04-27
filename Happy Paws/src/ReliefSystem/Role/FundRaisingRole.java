c54973d21a6777896a655972e444eb9cd05ec82e
jsher <jsher@10.110.74.194>
2022-04-27 12:56:06 -0400

    FundRaiser

---
+++ Happy Paws/src/ReliefSystem/Role/FundRaisingRole.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ReliefSystem.Role;
+
+import ReliefSystem.Ecosystem;
+import ReliefSystem.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+
+
+/**
+ *
+ * @author jsher
+ */
+public class FundRaisingRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Ecosystem system) {
+        return new FundraiserWorkArea(userProcessContainer, account, system);
+    }
+    
+}
