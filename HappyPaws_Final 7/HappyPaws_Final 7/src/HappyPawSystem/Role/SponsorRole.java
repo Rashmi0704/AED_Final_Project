0661aa98b72dbc0b3d2af0281c25d02b49ff18e7
Rashmi0704 <98605689+Rashmi0704@users.noreply.github.com>
2022-04-27 14:22:30 -0400

    adding Roles

---
+++ HappyPaws_Final 7/HappyPaws_Final 7/src/HappyPawSystem/Role/SponsorRole.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HappyPawSystem.Role;
+
+import javax.swing.JPanel;
+import HappyPawSystem.Ecosystem;
+import HappyPawSystem.UserAccount.UserAccount;
+import UserInterface.Sponsor.SponsorWorkArea;
+
+/**
+ *
+ * @author rashmisingh
+ */
+public class SponsorRole extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Ecosystem system) {
+        return new SponsorWorkArea(userProcessContainer, account, system);
+    }
+}
+
