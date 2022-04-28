c9ab84ee5e62b582fa40370853f4ecbe38224f3d
jsher <jsher@10.110.74.194>
2022-04-27 20:04:28 -0400

    VetRole

---
+++ Happy Paws/src/ReliefSystem/Role/VetRole.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ReliefSystem.Role;
+
+import ReliefSystem.Ecosystem;
+import ReliefSystem.UserAccount.UserAccount;
+import UserInterface.HospiitalAdmin.HospitalAdminWorkArea;
+import UserInterface.HospiitalAdmin.ManageVet;
+import UserInterface.Hospital.VetWorkarea;
+import javax.swing.JPanel;
+/**
+ *
+ * @author jsher
+ */
+public class VetRole extends Role{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Ecosystem system) {
+        return new VetWorkarea(userProcessContainer, system);
+    }
+}
